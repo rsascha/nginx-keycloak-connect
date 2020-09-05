eb2f3fed7a60c8a518a25dd8dec4b99d1e109a10
rsascha <sascha.rose@gmail.com>
2020-09-05 17:31:51 +0200

    backup

--- development-tools/webservice-nest/src/app.controller.ts
+++ development-tools/webservice-nest/src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request } from 'express';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Req() request: Request): string {
+    const token = request.headers["authorization"];
+    return this.appService.getHello(token);
   }
 }
--- development-tools/webservice-nest/src/app.service.ts
+++ development-tools/webservice-nest/src/app.service.ts
@@ -1,8 +1,37 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, Logger } from '@nestjs/common';
+import { GrantManager } from 'keycloak-auth-utils';
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private logger: Logger;
+
+  constructor() {
+    this.logger = new Logger("AppService");
+  }
+
+  getHello(token: string): string {
+
+    // {
+    //   "realm": "master",
+    //   "auth-server-url": "http://localhost:8080/auth/",
+    //   "ssl-required": "external",
+    //   "resource": "nginx-proxy",
+    //   "credentials": {
+    //     "secret": "5cbb59bd-e353-4528-889d-253f05ed7fc7"
+    //   },
+    //   "confidential-port": 0
+    // }
+
+    const config = {
+      realmUrl: "http://172.18.0.1:8080/auth/realms/master",
+      clientId: "nginx-proxy",
+      secret: "5cbb59bd-e353-4528-889d-253f05ed7fc7",
+    };
+
+    this.logger.log(`Receive request with ${token}`);
+    const gm = new GrantManager(config);
+    const result = gm.validateAccessToken(token);
+    this.logger.log(`Result ${result}`);
+
+    return result;
   }
 }
